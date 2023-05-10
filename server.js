2095a1ea45bdf4877389c151b4dc0ac0619d7db2
Gokul <81502324+Gokul07-coder@users.noreply.github.com>
2023-05-10 16:58:45 +0530

    Updated server.js

--- server.js
+++ server.js
@@ -16,19 +16,29 @@ let id = redisClient.address;
 console.log(id);
 
 app.get("/photos", async (req, res) => {
+  //checking for the server if contains the data will be outputed
+  
   redisClient.get("photos", async (error, photos) => {
     if (error) {
       console.log(error);
       res.status(500);
     } else {
+      //if available getting from the redis-server
       if (photos != null) {
         console.log("cache HIT !!!!!!");
+        
+        //it stores as an string so we are parsing
         return res.status(200).json(JSON.parse(photos));
-      } else {
+      }
+      
+      //else fetching the data and setting it in the server on second api call this wont work :)
+      else {
         console.log("cache misss !!!!");
         const { data } = await axios.get(
           "https://jsonplaceholder.typicode.com/photos"
         );
+        
+        //it accepts String format so we are converting into string
         redisClient.setex("photos", 3600, JSON.stringify(data));
         res.json(data);
       }
